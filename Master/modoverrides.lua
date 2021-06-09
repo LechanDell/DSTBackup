17566564054804068e34702d21bcea5e648948f6
root <root@Steam.192.168.50.1>
2021-06-09 16:24:03 +0000

    进度:21天

---
+++ Master/modoverrides.lua
@@ -0,0 +1,114 @@
+return {
+  ["workshop-1418746242"]={
+    ["configuration_options"]={ ["clearfont"]=true, ["eventplus"]=true, ["extratrans"]=true },
+    ["enabled"]=true 
+  },
+  ["workshop-1508510758"]={ ["configuration_options"]={  }, ["enabled"]=true },
+  ["workshop-1530801499"]={
+    ["configuration_options"]={ ["Hunger_Cost"]=1, ["Ownership"]=false, ["Sanity_Cost"]=1 },
+    ["enabled"]=true 
+  },
+  ["workshop-2078243581"]={
+    ["configuration_options"]={
+      ["Blue"]=0,
+      ["Display"]="target",
+      ["Green"]=0,
+      ["Projectile"]=true,
+      ["Red"]=1,
+      ["Type"]="hit" 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-2303894106"]={
+    ["configuration_options"]={
+      ["Language"]="AUTO",
+      ["coefficient of price"]=1,
+      ["harvest machine"]=true,
+      ["max store num"]=20,
+      ["refresh frequency"]=2,
+      ["seeding machine"]=true,
+      ["vending machine"]=true 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-2322014692"]={ ["configuration_options"]={  }, ["enabled"]=true },
+  ["workshop-347079953"]={
+    ["configuration_options"]={ ["DFV_Language"]="CN", ["DFV_MinimalMode"]="default" },
+    ["enabled"]=true 
+  },
+  ["workshop-356930882"]={
+    ["configuration_options"]={ ["uses"]=10000000, ["uses2"]=10000000, ["uses3"]=10000000 },
+    ["enabled"]=true 
+  },
+  ["workshop-375859599"]={
+    ["configuration_options"]={
+      ["divider"]=5,
+      ["random_health_value"]=0,
+      ["random_range"]=0,
+      ["send_unknwon_prefabs"]=false,
+      ["show_type"]=0,
+      ["unknwon_prefabs"]=1,
+      ["use_blacklist"]=true 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-378160973"]={
+    ["configuration_options"]={
+      ["ENABLEPINGS"]=true,
+      ["FIREOPTIONS"]=2,
+      ["OVERRIDEMODE"]=false,
+      ["SHAREMINIMAPPROGRESS"]=true,
+      ["SHOWFIREICONS"]=true,
+      ["SHOWPLAYERICONS"]=true,
+      ["SHOWPLAYERSOPTIONS"]=2 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-462434129"]={
+    ["configuration_options"]={
+      ["MOD_RESTART_ALLOW_KILL"]=true,
+      ["MOD_RESTART_ALLOW_RESTART"]=true,
+      ["MOD_RESTART_ALLOW_RESURRECT"]=true,
+      ["MOD_RESTART_CD_BONUS"]=0,
+      ["MOD_RESTART_CD_KILL"]=0,
+      ["MOD_RESTART_CD_MAX"]=0,
+      ["MOD_RESTART_CD_RESTART"]=0,
+      ["MOD_RESTART_CD_RESURRECT"]=0,
+      ["MOD_RESTART_FORCE_DROP_MODE"]=2,
+      ["MOD_RESTART_IGNORING_ADMIN"]=false,
+      ["MOD_RESTART_MAP_SAVE"]=2,
+      ["MOD_RESTART_RESURRECT_HEALTH"]=0,
+      ["MOD_RESTART_TRIGGER_MODE"]=1,
+      ["MOD_RESTART_WELCOME_TIPS"]=true,
+      ["MOD_RESTART_WELCOME_TIPS_TIME"]=6 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-501385076"]={
+    ["configuration_options"]={
+      ["quick_cook_on_fire"]=true,
+      ["quick_harvest"]=true,
+      ["quick_pick_cactus"]=true,
+      ["quick_pick_plant_normal_ground"]=true 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-666155465"]={
+    ["configuration_options"]={
+      ["chestB"]=-1,
+      ["chestG"]=-1,
+      ["chestR"]=-1,
+      ["food_estimation"]=1,
+      ["food_order"]=1,
+      ["food_style"]=1,
+      ["lang"]="chs",
+      ["show_food_units"]=1,
+      ["show_uses"]=1 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-831523966"]={
+    ["configuration_options"]={ ["soul_stack"]=20, ["stack_size"]=999 },
+    ["enabled"]=true 
+  } 
+}
